6cc3db8815409bbb2483b05d80ab231c48f6cc9e
Daisuke <d.suke.kida@gmail.com>
2022-12-18 11:00:46 +0900

    create random-cat

---
+++ src/pages/index.tsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react"
+import type { NextPage, GetServerSideProps } from 'next'
+
+const catImages: string[] = [
+  'https://cdn2.thecatapi.com/images/bpc.jpg',
+  'https://cdn2.thecatapi.com/images/eac.jpg',
+  'https://cdn2.thecatapi.com/images/6qi.jpg',
+]
+
+const randomCatImage = (): string => {
+  const index = Math.floor(Math.random() * catImages.length)
+  return catImages[index]
+}
+
+interface CatCategory {
+  id: number
+  name: string
+}
+
+interface SearchCatImage {
+  breeds: string[]
+  categories: CatCategory[]
+  id: string
+  url: string
+  width: number
+  height: number
+}
+
+type SearchCatImageResponse = SearchCatImage[]
+
+const fetchCatImage = async (): Promise<SearchCatImage> => {
+  const res = await fetch('https://api.thecatapi.com/v1/images/search')
+  const result = (await res.json()) as SearchCatImageResponse
+  return result[0]
+}
+
+interface IndexPageProps
+{
+  initialCatImageUrl: string
+}
+
+const IndexPage: NextPage<IndexPageProps> = ({ initialCatImageUrl }) => {
+  const [catImageUrl, setCatImageUrl] = useState(initialCatImageUrl)
+  // const [catImageUrl, setCatImageUrl] = useState(
+  //   'https://cdn2.thecatapi.com/images/bpc.jpg'
+  // )
+  const handleClick = async () => {
+    const image = await fetchCatImage()
+    setCatImageUrl(image.url)
+  }
+  return (
+    <>
+      <button onClick={handleClick}>今日のねこ</button>
+      <div style={{ marginTop: 8 }}>
+        <img src={catImageUrl} />
+      </div>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<
+  IndexPageProps
+  > = async () => {
+    const catImage = await fetchCatImage()
+    return {
+      props: {
+        initialCatImageUrl: catImage.url
+      }
+    }
+  }
+
+export default IndexPage
